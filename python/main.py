8b34144b0397040a24383d8bf8aebc7f92b7e8ef
Tricster <mediosrity@gmail.com>
2022-11-30 09:48:33 +0800

    Use wasmtimepy to generate python bindings.
    Signed-off-by: Tricster <mediosrity@gmail.com>

--- python/main.py
+++ python/main.py
@@ -2,9 +2,7 @@ from wasmtime import Engine, Store, Config
 from markdown import Component
 
 if __name__ == "__main__":
-    path = "/home/tric/codebase/component-model-demo/target/wasm32-unknown-unknown/release/app.wasm"
     text = "# head"
-
     config = Config()
     engine = Engine(config)
     store = Store(engine)
